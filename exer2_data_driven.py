f8ea37b53311201d4161dca48da975d5c01cc752
christinamanara <manara1024@gmail.com>
2022-06-10 14:57:50 +0300

    10/6

--- exer2_data_driven.py
+++ exer2_data_driven.py
@@ -1,5 +1,6 @@
 ###############################  Numerical Laplace Equation Solution using Finite Difference Method  ###############################
 from itertools import count
+from unittest import result
 from scipy.linalg import block_diag
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,6 +14,7 @@ dim_X = 40
 dim_Y = 40
 delta = 1
 max_Iter = 1200
+h = delta/dim_X # or dim_Y it does not matter, type -> h=1/40=Δx=Δy
 
 def K(nblock):
     # Diagonal has -4 in every cell 
@@ -66,7 +68,7 @@ def solve_equation(K, b, nblock, Temp):
     u = np.linalg.solve(K, b)
     T = u.reshape((nblock, nblock))
     T_filled = embed(T, Temp)
-    print("Tfull = \n", T_filled)
+    print("T_filled = \n", T_filled)
     return T
 
 
@@ -76,24 +78,48 @@ solve_equation(K_arr, b_vec, nblock, Temp)
 
 # Part B
 
-def f_x_y(h, r):
+def f_x_y(h):
     f = []
     for i in range(1, dim_X-1, delta):
         for j in range(1, dim_Y-1, delta):
+            r = np.random.normal(mean, std)
             res = (h ** 2) * 100.0 * np.exp(-((i/N - 0.55) ** 2 + (j/N - 0.45) ** 2 ) / r)
-            print(res)
             f.append(res)
     return f
        
 
 mid_point = int((nblock**2 - 1) / 2)
+mean = 0.05
+std = 0.005
+K_arr_inv = np.linalg.inv(K_arr)
+results = []
 for i in range(max_Iter):
-    
+    tmp = f_x_y(h)
+    simulation = np.dot(K_arr_inv, tmp)
+    results.append(simulation)
+
 
 
 print(mid_point)
 
-
+def plot_samples(chain, log_prob, ax, orientation='vertical', normalize=True,
+                 xlims=(-5, 5), legend=True):
+    from scipy.integrate import quad
+    
+    ax.hist(chain, bins=50, density=True, label="Monte Carlo Samples",
+           orientation=orientation)
+    # we numerically calculate the normalization constant of our PDF
+    if normalize:
+        Z, _ = quad(lambda x: np.exp(log_prob(x)), -np.inf, np.inf)
+    else:
+        Z = 1.0
+    xses = np.linspace(xlims[0], xlims[1], 1000)
+    yses = [np.exp(log_prob(x)) / Z for x in xses]
+    if orientation == 'horizontal':
+        (yses, xses) = (xses, yses)
+    ax.plot(xses, yses, label="true distribution")
+    if legend:
+        ax.legend(frameon=False)
 # # Maximum iteration
 # max_Iter = 100
 
