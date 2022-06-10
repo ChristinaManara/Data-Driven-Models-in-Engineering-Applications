58ed0e106480243e55ee79f36a153f356bb9f40c
christinamanara <manara1024@gmail.com>
2022-06-10 22:35:12 +0300

    10/6

--- exer2_data_driven.py
+++ exer2_data_driven.py
@@ -1,4 +1,5 @@
 ###############################  Numerical Laplace Equation Solution using Finite Difference Method  ###############################
+from difflib import restore
 from itertools import count
 from unittest import result
 from scipy.linalg import block_diag
@@ -13,7 +14,7 @@ nblock = N - 2
 dim_X = 40
 dim_Y = 40
 delta = 1
-max_Iter = 1200
+max_Iter = 10000
 h = delta/dim_X # or dim_Y it does not matter, type -> h=1/40=Δx=Δy
 
 def K(nblock):
@@ -73,16 +74,42 @@ def solve_equation(K, b, nblock, Temp):
 
 
 K_arr = K(nblock)
+print("S", len(K_arr))
 b_vec = b(nblock, Temp)
-solve_equation(K_arr, b_vec, nblock, Temp)
+T_filled = solve_equation(K_arr, b_vec, nblock, Temp)
+
+
+
+# Colorplot 
+def colorplot(T_filled, N, h):
+    x = y = np.linspace(0, 1, N-2)
+
+
+    X, Y = np.meshgrid(x,y)
+    h = plt.contourf(X, Y, T_filled)
+    plt.axis('scaled')
+    plt.colorbar()
+    plt.show()
+    
+    # plt.figure(1)
+    # plt.clf()
+    # plt.pcolor(X, Y, T_filled)
+    # plt.axis("scaled")
+    # plt.colorbar()
+    # plt.xlabel("x (m)")
+    # plt.ylabel("y (m)")
+    # plt.title("T(x,y) on %dx%d grid" % (N,N))
+    # plt.show()
+
+# colorplot(T_filled, N, h)
+
 
 # Part B
 
-def f_x_y(h):
+def f_x_y(h, r):
     f = []
     for i in range(1, dim_X-1, delta):
         for j in range(1, dim_Y-1, delta):
-            r = np.random.normal(mean, std)
             res = (h ** 2) * 100.0 * np.exp(-((i/N - 0.55) ** 2 + (j/N - 0.45) ** 2 ) / r)
             f.append(res)
     return f
@@ -94,77 +121,29 @@ std = 0.005
 K_arr_inv = np.linalg.inv(K_arr)
 results = []
 for i in range(max_Iter):
-    tmp = f_x_y(h)
+    r = np.random.normal(mean, std)
+    tmp = f_x_y(h, r)
     simulation = np.dot(K_arr_inv, tmp)
     results.append(simulation)
 
-
-
 print(mid_point)
 
-def plot_samples(chain, log_prob, ax, orientation='vertical', normalize=True,
-                 xlims=(-5, 5), legend=True):
-    from scipy.integrate import quad
+
+def plot_samples(results, ax, orientation='vertical'):
     
-    ax.hist(chain, bins=50, density=True, label="Monte Carlo Samples",
+    ax.hist(results, bins=50, density=True, label="Monte Carlo Samples",
            orientation=orientation)
-    # we numerically calculate the normalization constant of our PDF
-    if normalize:
-        Z, _ = quad(lambda x: np.exp(log_prob(x)), -np.inf, np.inf)
-    else:
-        Z = 1.0
-    xses = np.linspace(xlims[0], xlims[1], 1000)
-    yses = [np.exp(log_prob(x)) / Z for x in xses]
-    if orientation == 'horizontal':
-        (yses, xses) = (xses, yses)
-    ax.plot(xses, yses, label="true distribution")
-    if legend:
-        ax.legend(frameon=False)
-# # Maximum iteration
-# max_Iter = 100
 
-# # Boundaries 
-# T_left = 0
-# T_right = 0
-# T_bottom = 0
-# T_top = 100
+plt.figure(2)  
+fig, ax = plt.subplots()
+plot_samples(results, ax)
+ax.set_yticks(())
+plt.show()
 
-# # Dimensions of plate -> 1m x 1m and delta
-# dim_X = 40
-# dim_Y = 40
-# delta = 1
-# h = delta/dim_X # or dim_Y it does not matter, type -> h=1/40=Δx=Δy
-# r = 0.05 # follow normal distribution r ~ N(0.05, 0.005)
-
-# # Initial guess of interior grid
-# T_guess = 30
-
-# # Set array size and set the interior value with Tguess
-# T = np.empty((dim_X, dim_Y))
-# T.fill(T_guess)
-
-# # Set Boundary condition
-# T[(dim_Y-1):, :] = T_top
-# T[:1, :] = T_bottom
-# T[:, (dim_X-1):] = T_right
-# T[:, :1] = T_left
-
-# # Set array b 
-# # b = np.empty(dim_X * dim_Y)
-# # counter = 0
+# Part C
 
 
 
-# # Iteration (We assume that the iteration is convergence in maxIter = 500)
-# # for iteration in range(0, max_Iter):
-# #     for i in range(1, dim_X-1, delta):
-# #         for j in range(1, dim_Y-1, delta):
-# #             T[i, j] = 0.25 * (T[i+1][j] + T[i-1][j] + T[i][j+1] + T[i][j-1])
-# #     if iteration == 1:
-# #         print(T)
-# #         break
-
-# # print("Iteration finished")
 
 
 
